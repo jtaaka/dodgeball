57a231d4f025b7c35164f5a3fb65d320eef1cb53
Nasahuta <joni.alanko@cs.tamk.fi>
2018-02-22 12:27:45 +0200

    testerooni

--- core/src/tamk/fi/polttopallopeli/Peli.java
+++ core/src/tamk/fi/polttopallopeli/Peli.java
@@ -21,6 +21,7 @@ public class Peli extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		Gdx.app.log("TESTI", "TESTI");
+		//WHAT IS THIS
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
