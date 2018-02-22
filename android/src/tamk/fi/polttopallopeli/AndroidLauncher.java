82ca7787a1abfb140277d74893c92996d75fe1be
jtaaka <juho.taakala@cs.tamk.fi>
2018-02-22 12:15:15 +0200

    Initial commit

---
+++ android/src/tamk/fi/polttopallopeli/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package tamk.fi.polttopallopeli;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tamk.fi.polttopallopeli.Peli;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Peli(), config);
+	}
+}
---
+++ desktop/src/tamk/fi/polttopallopeli/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tamk.fi.polttopallopeli.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tamk.fi.polttopallopeli.Peli;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Peli(), config);
+	}
+}
