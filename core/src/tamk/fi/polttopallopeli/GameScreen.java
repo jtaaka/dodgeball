236bb8404e8a3938e8b769822d48d3271a17fef8
Nasahuta <joni.alanko@cs.tamk.fi>
2018-02-23 12:33:23 +0200

    Game background

--- core/src/tamk/fi/polttopallopeli/GameScreen.java
+++ core/src/tamk/fi/polttopallopeli/GameScreen.java
@@ -3,15 +3,21 @@ package tamk.fi.polttopallopeli;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
     private SpriteBatch batch;
     private Dodgeball host;
+    private Texture backgroundGame;
+    private OrthographicCamera camera;
 
     public GameScreen (Dodgeball host) {
         this.host = host;
         batch = host.getBatch();
+
+        backgroundGame = new Texture("game.jpg");
     }
 
     @Override
@@ -23,8 +29,11 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Dodgeball.WORLD_WIDTH, Dodgeball.WORLD_HEIGHT);
 
         batch.begin();
+        batch.draw(backgroundGame,0,0, Dodgeball.WORLD_WIDTH, Dodgeball.WORLD_HEIGHT);
         batch.end();
     }
 
