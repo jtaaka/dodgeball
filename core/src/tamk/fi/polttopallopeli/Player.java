4990a7829414842b1cbc3dc7ae5bb8403f421ae0
Joni <joni.alanko@cs.tamk.fi>
2018-03-29 01:07:57 +0300

    Added proper player image. Added head body. Added y-accelerator calibration.

--- core/src/tamk/fi/polttopallopeli/Player.java
+++ core/src/tamk/fi/polttopallopeli/Player.java
@@ -11,13 +11,16 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.joints.WeldJointDef;
 
 public class Player extends Sprite {
     private World world;
     protected Body body;
+    protected Body head;
     private SpriteBatch batch;
     private Vector2 vector;
 
@@ -29,10 +32,12 @@ public class Player extends Sprite {
     private Animation<TextureRegion> playerAnime;
     private float currentFrameTime;
 
-    private float tabletAccelerometerSetting;
+    private float tabletAccelerometerSettingZ;
+    private float tabletAccelerometerSettingY;
 
     public Player(World world, SpriteBatch batch) {
-        super(new Texture("walk.png"));
+        //super(new Texture("walk.png"));
+        super(new Texture("hahmo.png"));
 
         healthTexture = new Texture("healths.png");
         invulnerability = 0;
@@ -43,13 +48,14 @@ public class Player extends Sprite {
         TextureRegion[] healthFrames = healthsTo1D(temp);
         healthAnimation = new Animation<TextureRegion>(1/10f, healthFrames);
 
-
+/*
         TextureRegion[][] tmp = TextureRegion.split(getTexture(), getTexture().getWidth() / 8,
                 getTexture().getHeight() / 8);
         TextureRegion[] playerFrames = transformTo1D(tmp);
         playerAnime = new Animation<TextureRegion>(1/10f, playerFrames);
-
-        setSize(getWidth() / 800f, getHeight() / 800f);
+*/
+        //setSize(getWidth() / 800f, getHeight() / 800f);
+        setSize(getWidth() / 200f, getHeight() / 200f);
         setOriginCenter();
 
         currentFrameTime = 0;
@@ -68,27 +74,45 @@ public class Player extends Sprite {
         bodyDef.position.set((Dodgeball.WORLD_WIDTH / 2f),
                 (Dodgeball.WORLD_HEIGHT / 2f));
         bodyDef.fixedRotation = true;
-        bodyDef.linearDamping = 0.8f; //0.5f OG
+        bodyDef.linearDamping = 1f; //0.5f OG
 
         body = world.createBody(bodyDef);
 
-        PolygonShape shape = new PolygonShape();
+        bodyDef.position.set((Dodgeball.WORLD_WIDTH / 2f),
+                (Dodgeball.WORLD_HEIGHT / 1.8f));
+        head = world.createBody(bodyDef);
 
-        shape.setAsBox(getWidth() / 8, getHeight() / 8 * 2);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(getWidth() / 8.5f, getHeight() / 5.5f);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
-        fixtureDef.density = 0.10f; // 0.05f OG
+        fixtureDef.density = 0.20f; // 0.05f OG
         fixtureDef.friction = 1f;
         fixtureDef.filter.categoryBits = Dodgeball.OBJECT_PLAYER;
         fixtureDef.filter.maskBits = Dodgeball.OBJECT_WALL | Dodgeball.OBJECT_BALL;
 
         body.createFixture(fixtureDef).setUserData(this);
 
+        CircleShape circle = new CircleShape();
+        circle.setRadius(0.25f);
+        fixtureDef.shape = circle;
+
+        head.createFixture(fixtureDef).setUserData(this);
+
+        WeldJointDef weldJointDef = new WeldJointDef();
+        weldJointDef.bodyA = body;
+        weldJointDef.bodyB = head;
+        weldJointDef.localAnchorA.add(0.02f, getHeight() / 8 * 3.5f);
+        world.createJoint(weldJointDef);
+
         //Kalibroi lähtöasennon, ei vaikuta desktopilla
-        tabletAccelerometerSetting = 0;
+        tabletAccelerometerSettingZ = 0;
+        tabletAccelerometerSettingY = 0;
         if (Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer)) {
-            tabletAccelerometerSetting = Gdx.input.getAccelerometerZ();
+            tabletAccelerometerSettingZ = Gdx.input.getAccelerometerZ();
+            tabletAccelerometerSettingY = Gdx.input.getAccelerometerY();
         }
 
         shape.dispose();
@@ -195,7 +219,7 @@ public class Player extends Sprite {
     float accelY;
 
     public void playerMove(float delta) {
-        float initialFrameTime = getDirectionalFrameTime();
+        //float initialFrameTime = getDirectionalFrameTime();
 
         batch.begin();
 
@@ -209,15 +233,26 @@ public class Player extends Sprite {
             currentFrameTime += delta;
         }
 
+        /*
         if (initialFrameTime + currentFrameTime > initialFrameTime + 1/10f * 8f) {
             currentFrameTime = 0;
         }
+        */
 
-        TextureRegion currentFrame = playerAnime.getKeyFrame(initialFrameTime + currentFrameTime, true);
+        //TextureRegion currentFrame = playerAnime.getKeyFrame(initialFrameTime + currentFrameTime, true);
+
         //batch.draw(currentFrame, body.getPosition().x - getWidth() / 160f, body.getPosition().y - getHeight() / 220f,
         //       getWidth() / 80f, getHeight() / 80f);
+        // ^^ OLD? ^^
+
+        /*
         batch.draw(currentFrame, body.getPosition().x - getWidth() / 2f, body.getPosition().y - getHeight() / 2.4f,
                 getWidth(), getHeight());
+                */
+
+        setPosition(body.getPosition().x - getWidth() / 2.3f,body.getPosition().y - getHeight() / 4);
+        draw(batch);
+
         vector.set(0, 0);
 
         // For testing purposes on computer
@@ -238,15 +273,15 @@ public class Player extends Sprite {
         }
 
         // Accelerometer testing for tablet
-        accelY = Gdx.input.getAccelerometerY();
-        accelZ = Gdx.input.getAccelerometerZ() - tabletAccelerometerSetting; //ei vaikuta Desktopilla
+        accelY = Gdx.input.getAccelerometerY() - tabletAccelerometerSettingY;
+        accelZ = Gdx.input.getAccelerometerZ() - tabletAccelerometerSettingZ; //ei vaikuta Desktopilla
 
         if (!MathUtils.isZero(accelY, 0.5f)) {
-            vector.x = accelY * delta;
+            vector.x = accelY * delta * 2f;
         }
 
         if (!MathUtils.isZero(accelZ, 0.5f)) {
-            vector.y = accelZ * delta;
+            vector.y = accelZ * delta * 2f;
         }
 
         body.applyForceToCenter(vector, true);
