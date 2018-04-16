e09571404267f2660e922dc10ec9c7377e0b2fff
Nasahuta <joni.alanko@cs.tamk.fi>
2018-04-16 13:21:20 +0300

    Fixed center of player drawing.

--- core/src/tamk/fi/polttopallopeli/CenterOfPlayer.java
+++ core/src/tamk/fi/polttopallopeli/CenterOfPlayer.java
@@ -1,5 +1,6 @@
 package tamk.fi.polttopallopeli;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,20 +10,20 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
-/**
- * Created by Joni on 22.3.2018.
- */
-
 public class CenterOfPlayer extends Sprite {
     private Array<CenterOfPlayerObject> center;
     private ShapeRenderer shape;
+    private Vector2 calculatedCenter;
+    private Vector2 lastPoint;
 
     public CenterOfPlayer() {
         super(new Texture("peruspalloTESTI.png"));
-        setSize(getWidth() / 300f, getHeight() / 300f);
+        setSize(getWidth() / 200f, getHeight() / 200f);
         setOriginCenter();
         center = new Array<CenterOfPlayerObject>();
         shape = new ShapeRenderer();
+        calculatedCenter = new Vector2();
+        lastPoint = new Vector2();
     }
 
     public void modify(float x, float y) {
@@ -31,7 +32,7 @@ public class CenterOfPlayer extends Sprite {
 
         for (CenterOfPlayerObject heatMapObject : center) {
             if (heatMapObject.point.equals(object.point)) {
-                heatMapObject.addHeatValue();
+                //heatMapObject.addHeatValue();
                 //Gdx.app.log(getClass().getSimpleName(), "Wörk IF?" );
                 return;
             }
@@ -46,12 +47,12 @@ public class CenterOfPlayer extends Sprite {
 
         CenterOfPlayerObject(float x, float y) {
             point = new Vector2(Dodgeball.WORLD_WIDTH / 2f + x,Dodgeball.WORLD_HEIGHT / 2f + y);   //OG point = new Vector2(round(x), round(y));
-            color = new Color(0.1f, 0, 0, 0.1f);
+            //color = new Color(0.1f, 0, 0, 0.1f);
             //Gdx.app.log(getClass().getSimpleName(), "Wörk? POINT" + point );
         }
 
         void addHeatValue() {
-            color.add(0.017f, 0, 0, 0.015f);
+            //color.add(0.017f, 0, 0, 0.015f);
             //Gdx.app.log(getClass().getSimpleName(), "Wörk? " + color);
         }
 
@@ -60,20 +61,28 @@ public class CenterOfPlayer extends Sprite {
         }
     }
 
+    public void calculatedCenter(Vector2 calculatedCenter) {
+        this.calculatedCenter = calculatedCenter;
+    }
+
     public void draw(Batch batch, Camera camera) {
-        Vector2 lastPoint = new Vector2();
         lastPoint.x = Dodgeball.WORLD_WIDTH / 2f;
         lastPoint.y = Dodgeball.WORLD_HEIGHT / 2f;
+
         for (CenterOfPlayerObject heat : center) {
-            setColor(heat.color);
-            //setPosition(Dodgeball.WORLD_WIDTH / 2f + heat.point.x - getWidth() / 2f, Dodgeball.WORLD_HEIGHT / 2f + heat.point.y - getHeight() / 2f);
             shape.setProjectionMatrix(camera.combined);
             shape.begin(ShapeRenderer.ShapeType.Line);
             shape.line(heat.point, lastPoint);
             lastPoint = heat.point;
             shape.end();
-            super.draw(batch);
         }
+batch.begin();
+        setColor(1,0,0,1);
+        setPosition(Dodgeball.WORLD_WIDTH / 2f + calculatedCenter.x - getWidth() / 2f, Dodgeball.WORLD_HEIGHT / 2f + calculatedCenter.y - getHeight() / 2f);
+        setPosition(Dodgeball.WORLD_WIDTH / 2f, Dodgeball.WORLD_HEIGHT / 2f);
+        draw(batch);
+        batch.end();
+        Gdx.app.log(getClass().getSimpleName(), "x: " + (Dodgeball.WORLD_WIDTH / 2f + calculatedCenter.x - getWidth() / 2f) + " y: " + (Dodgeball.WORLD_HEIGHT / 2f + calculatedCenter.y - getHeight() / 2f));
     }
 
     public void dispose() {
